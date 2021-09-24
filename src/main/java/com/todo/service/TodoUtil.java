88e4a20db7d355386c0109d8be26a98c7aad8943
zionhann <hanzion1108@gmail.com>
2021-09-24 19:52:58 +0900

    Improve add and edit commands
    They read the current line instead of the next token

--- src/main/java/com/todo/service/TodoUtil.java
+++ src/main/java/com/todo/service/TodoUtil.java
@@ -16,14 +16,14 @@ public class TodoUtil {
 				+ "========== 할 일 추가\n"
 				+ "할 일 입력하세요.\n");
 		
-		title = sc.next();
+		title = sc.nextLine();
 		if (list.isDuplicate(title)) {
 			System.out.printf("이미 존재하는 할 일입니다.");
 			return;
 		}
 		
 		System.out.println("메모 입력");
-		desc = sc.next();
+		desc = sc.nextLine();
 		
 		TodoItem t = new TodoItem(title, desc);
 		list.addItem(t);
@@ -55,21 +55,21 @@ public class TodoUtil {
 				+ "========== 할 일 수정\n"
 				+ "수정할 할 일을 입력하세요.\n"
 				+ "\n");
-		String title = sc.next().trim();
+		String title = sc.nextLine().trim();
 		if (!l.isDuplicate(title)) {
 			System.out.println("해당 할 일이 존재하지 않습니다.");
 			return;
 		}
 
 		System.out.println("새로운 할 일을 입력하세요.");
-		String new_title = sc.next().trim();
+		String new_title = sc.nextLine().trim();
 		if (l.isDuplicate(new_title)) {
 			System.out.println("이미 존재하는 할 일입니다.");
 			return;
 		}
 		
 		System.out.println("새로운 메모를 입력하세요.");
-		String new_description = sc.next().trim();
+		String new_description = sc.nextLine().trim();
 		for (TodoItem item : l.getList()) {
 			if (item.getTitle().equals(title)) {
 				l.deleteItem(item);
