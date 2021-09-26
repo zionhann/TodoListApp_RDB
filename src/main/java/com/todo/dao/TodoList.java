8f570b34eccc7dd0b9c69a7e8ce331447968a0f8
zionhann <hanzion1108@gmail.com>
2021-09-27 00:31:54 +0900

    Refactor

--- src/main/java/com/todo/dao/TodoList.java
+++ src/main/java/com/todo/dao/TodoList.java
@@ -36,10 +36,9 @@ public class TodoList {
 	}
 
 	public void listAll() {
-		System.out.println("\n"
-				+ "inside list_All method\n");
-		for (TodoItem myitem : list) {
-			System.out.println(myitem.getTitle() + "(" + myitem.getDesc() + ")");
+
+		for (TodoItem item : list) {
+			System.out.println(list.indexOf(item)+1 + "." + item.toString());
 		}
 	}
 	
